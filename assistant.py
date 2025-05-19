93dc567219b7111e76e2146586674d59c26fd36c
Mohamed Zeina <toxiczeina@gmail.com>
2025-05-19 19:28:56 +0300

    added system prompt for the llms

--- assistant.py
+++ assistant.py
@@ -16,3 +16,8 @@ openai = OpenAI()
 google_api_key = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=google_api_key)
 gemini = genai.GenerativeModel(MODEL_GEMINI)
+
+
+sysPrompt = "You are an expert at answering technical questions. "
+sysPrompt += "Provide clear, accurate, and well-structured explanations that help the user understand complex concepts easily. "
+sysPrompt += "If you do not know something then make it clear to the user that you do not have an answer."
