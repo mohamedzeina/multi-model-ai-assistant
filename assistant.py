4b1819bb2186697c5a5a371d4f4f5a1d90f82a4f
Mohamed Zeina <toxiczeina@gmail.com>
2025-05-19 19:31:02 +0300

    added talker function that converts text to speech

--- assistant.py
+++ assistant.py
@@ -1,5 +1,8 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+from pydub import AudioSegment
+from pydub.playback import play
+from io import BytesIO
 import os
 import google.generativeai as genai
 
@@ -21,3 +24,17 @@ gemini = genai.GenerativeModel(MODEL_GEMINI)
 sysPrompt = "You are an expert at answering technical questions. "
 sysPrompt += "Provide clear, accurate, and well-structured explanations that help the user understand complex concepts easily. "
 sysPrompt += "If you do not know something then make it clear to the user that you do not have an answer."
+
+
+def talker(message):
+    try:
+        response = openai.audio.speech.create(
+            model="tts-1",
+            voice="onyx",
+            input=message
+        )
+        audio_stream = BytesIO(response.content)
+        audio = AudioSegment.from_file(audio_stream, format="mp3")
+        play(audio)
+    except Exception as e:
+        print(f"[Talker Error] {e}")
